b21a35132bc7ccd393554d210c3b96dc731a2718
kahousami <kaho.usami@gmail.com>
2020-04-12 08:51:23 +0000

    add pagination

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def top
   	@posts = Post.all.reverse_order
-  	@posts = Post.page(params[:page]).per(18)
+  	@posts = Post.page(params[:page]).per(15)
   end
 
   def about
